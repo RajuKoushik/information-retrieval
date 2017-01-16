65b8b57885fe4cfb69c269e31ec9b8ff3db15be0
rajukoushik <g.rajukoushik@gmail.com>
2017-01-16 12:30:52 +0530

    module to parse an xml into a string without tags.

--- parse-xml.py
+++ parse-xml.py
@@ -1,6 +1,17 @@
 #parsing an xml file and outputting a txt file
 
 import xml.etree.ElementTree as ET
-tree = ET.parse('hindi-document-00001.xml')
-notags = ET.tostring(tree.getroot(), encoding='utf-8',method='text')
-print(notags)
+#tree = ET.parse('hindi-document-00001.xml')
+#notags = ET.tostring(tree.getroot(), encoding='utf-8',method='text')
+#print(notags)
+
+
+def parsexml(filename):
+    "parses an xml and outputs the txt file"
+    tree = ET.parse('hindi-document-00001.xml')
+    notags = ET.tostring(tree.getroot(), encoding='utf-8', method='text')
+    print 'test notags'
+    print(notags)
+    return notags
+
+parsexml('hindi-document-00001.xml')
